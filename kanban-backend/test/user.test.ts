84b3c003a82f86f8aea61ae7621a9939380e03a8
Oskari Juvakka <oskari.juvakka@gmail.com>
2023-09-26 22:48:39 +0300

    Feature-4: add password hashing

--- kanban-backend/test/user.test.ts
+++ kanban-backend/test/user.test.ts
@@ -13,7 +13,7 @@ describe('User tests', () => {
     expect(response.status).toBe(200);
   });
 
-  test('Create user', async () => {
+  test('Create user and return name and username, not return hashed password', async () => {
     const response = await api
       .post('/api/users')
       .send({ username: 'John', name: 'John Doe', password: 'password' });
@@ -21,5 +21,6 @@ describe('User tests', () => {
     expect(response.status).toBe(200);
     expect(response.body.name).toBe('John Doe');
     expect(response.body.username).toBe('John');
+    expect(response.body.password).toBeUndefined();
   });
 });
