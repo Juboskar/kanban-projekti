f09959e6a27e83e082a4f43fd15bb0e07372f5a8
Oskari Juvakka <oskari.juvakka@gmail.com>
2023-10-09 00:17:28 +0300

    Feature-4: fix input styles and fields, add forwardRef

--- kanban-frontend/src/components/TextInput/TextInput.tsx
+++ kanban-frontend/src/components/TextInput/TextInput.tsx
@@ -1,52 +1,49 @@
-import React, { ChangeEventHandler } from 'react';
+import React, { ChangeEventHandler, forwardRef } from 'react';
 import RequiredAsterisk from '../RequiredAsterisk';
 
 interface TextInputProps {
   label?: string;
-  value: string;
+  name?: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
-  type?: string;
+  onBlur: ChangeEventHandler<HTMLInputElement>;
+  type?: 'text' | 'password' | 'email';
   required?: boolean;
 }
-const TextInput = ({
-  label,
-  value,
-  onChange,
-  type,
-  required,
-}: TextInputProps) => {
-  if (!type) {
-    type = 'text';
-  }
-
-  return (
-    <div className="p-4">
-      <div className="group relative">
-        <label>
-          <input
-            required={required}
-            type={type}
-            className="peer w-full rounded border-2 border-black p-2"
-            value={value}
-            onChange={onChange}
-          />
-          <span
-            className="text-md pointer-events-none absolute left-3 top-2.5 
-              text-gray-400 transition peer-focus:-translate-y-6 
-              peer-focus:bg-white peer-focus:px-1 peer-focus:pt-1 
-              peer-focus:text-sm peer-focus:text-black 
-              peer-[:not([value=''])]:-translate-y-6  
-              peer-[:not([value=''])]:bg-white  peer-[:not([value=''])]:px-1 
-              peer-[:not([value=''])]:pt-1 peer-[:not([value=''])]:text-sm 
-              peer-[:not([value=''])]:text-black"
-          >
-            {label}
-            {required && <RequiredAsterisk />}
-          </span>
-        </label>
+const TextInput = forwardRef(
+  ({ label, name, onChange, onBlur, type, required }: TextInputProps, ref) => {
+    return (
+      <div className="p-4">
+        <div className="group relative">
+          <label>
+            <input
+              name={name}
+              ref={ref as any}
+              required={required}
+              type={type ? type : 'text'}
+              className="peer w-full rounded border-2 border-black p-2"
+              onChange={onChange}
+              onBlur={onBlur}
+              placeholder=" "
+            />
+            <span
+              className="text-md pointer-events-none absolute left-3 top-2.5 
+              text-gray-400 transition placeholder-shown:text-red-700 
+              peer-[:not(:placeholder-shown)]:-translate-y-6  
+              peer-[:not(:placeholder-shown)]:bg-white 
+              peer-[:not(:placeholder-shown)]:px-1 
+              peer-[:not(:placeholder-shown)]:pt-1 
+              peer-[:not(:placeholder-shown)]:text-sm 
+              peer-[:not(:placeholder-shown)]:text-black"
+            >
+              {label}
+              {required && <RequiredAsterisk />}
+            </span>
+          </label>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
+TextInput.displayName = 'TextInput';
 export default TextInput;
