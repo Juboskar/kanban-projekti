b5c2d1034e39954884a673c535f1759e71ba5127
Oskari Juvakka <oskari.juvakka@gmail.com>
2024-03-07 18:43:52 +0200

    Feature-4: format classes

--- kanban-frontend/src/pages/LandingPage/components/CardNavigation/CardNavigation.tsx
+++ kanban-frontend/src/pages/LandingPage/components/CardNavigation/CardNavigation.tsx
@@ -11,19 +11,19 @@ const CardNavigation = ({ setShowLogin, showLogin }: Props) => {
       <div className="relative top-2">
         <nav
           className="m-auto grid max-w-sm grid-flow-col justify-stretch 
-          overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600 bg-white"
+          overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600
+          bg-white"
         >
           <button
-            className="flex skew-x-[-45deg] justify-center border-b-2  
-             border-solid border-gray-600 p-4 
-            hover:font-bold "
+            className="flex skew-x-[-45deg] justify-center border-b-2 
+            border-solid border-gray-600 p-4 hover:font-bold"
             onClick={() => setShowLogin(false)}
           >
             <span className="skew-x-[45deg]">SIGN UP</span>
           </button>
           <div
             className="flex skew-x-[-45deg] justify-center  border-l-2 
-             border-gray-600 p-4 shadow-[-3px_0px_2px_0px] shadow-gray-500"
+            border-gray-600 p-4 shadow-[-3px_0px_2px_0px] shadow-gray-500"
           >
             <span className="skew-x-[45deg]">LOG IN</span>
           </div>
@@ -36,18 +36,18 @@ const CardNavigation = ({ setShowLogin, showLogin }: Props) => {
     <div className="relative top-2">
       <nav
         className="m-auto grid max-w-sm grid-flow-col justify-stretch 
-            overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600 bg-white"
+        overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600
+        bg-white"
       >
         <div
-          className="flex skew-x-[45deg] justify-center border-r-2  
-              border-solid border-gray-600 p-4 shadow-[3px_0px_2px_0px] shadow-gray-500 
-              "
+          className="flex skew-x-[45deg] justify-center border-r-2 border-solid
+          border-gray-600 p-4 shadow-[3px_0px_2px_0px] shadow-gray-500"
         >
           <span className="skew-x-[-45deg]">SIGN UP</span>
         </div>
         <button
-          className="flex skew-x-[45deg] justify-center 
-              border-b-2 border-gray-600 p-4 hover:font-bold"
+          className="flex skew-x-[45deg] justify-center border-b-2 
+          border-gray-600 p-4 hover:font-bold"
           onClick={() => setShowLogin(true)}
         >
           <span className="skew-x-[-45deg]">LOG IN</span>
