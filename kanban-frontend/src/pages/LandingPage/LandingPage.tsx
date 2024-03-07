6b75936f237c9d539bff6bb1503c9eb4332d0e76
Oskari Juvakka <oskari.juvakka@gmail.com>
2024-03-07 17:35:11 +0200

    Feature-4: add navigation to card to select login view

--- kanban-frontend/src/pages/LandingPage/LandingPage.tsx
+++ kanban-frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignUpForm from './components/SignUpForm';
 import Card from '../../components/Card';
+import CardNavigation from './components/CardNavigation';
 
 const LandingPage = () => {
+  const [showLogin, setShowLogin] = useState<boolean>(false);
+
   return (
     <div>
       <h1 className="max-w-sm p-4 align-baseline text-4xl">
         Kanban tool for project management
       </h1>
       <div className="py-16">
+        <CardNavigation setShowLogin={setShowLogin} showLogin={showLogin} />
         <Card>
-          <SignUpForm />
+          {!showLogin && <SignUpForm />}
+          {showLogin && <p>todo login</p>}
         </Card>
       </div>
     </div>
---
+++ kanban-frontend/src/pages/LandingPage/components/CardNavigation/CardNavigation.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+type Props = {
+  setShowLogin: (showLogin: boolean) => void;
+  showLogin: boolean;
+};
+
+const CardNavigation = ({ setShowLogin, showLogin }: Props) => {
+  if (showLogin) {
+    return (
+      <div className="relative top-2">
+        <nav
+          className="m-auto grid max-w-sm grid-flow-col justify-stretch 
+          overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600 bg-white"
+        >
+          <button
+            className="flex skew-x-[-45deg] justify-center border-b-2  
+             border-solid border-gray-600 p-4 
+            hover:font-bold "
+            onClick={() => setShowLogin(false)}
+          >
+            <span className="skew-x-[45deg]">SIGN UP</span>
+          </button>
+          <div
+            className="flex skew-x-[-45deg] justify-center  border-l-2 
+             border-gray-600 p-4 shadow-[-3px_0px_2px_0px] shadow-gray-500"
+          >
+            <span className="skew-x-[45deg]">LOG IN</span>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative top-2">
+      <nav
+        className="m-auto grid max-w-sm grid-flow-col justify-stretch 
+            overflow-hidden rounded-t-lg border-2 border-b-0 border-gray-600 bg-white"
+      >
+        <div
+          className="flex skew-x-[45deg] justify-center border-r-2  
+              border-solid border-gray-600 p-4 shadow-[3px_0px_2px_0px] shadow-gray-500 
+              "
+        >
+          <span className="skew-x-[-45deg]">SIGN UP</span>
+        </div>
+        <button
+          className="flex skew-x-[45deg] justify-center 
+              border-b-2 border-gray-600 p-4 hover:font-bold"
+          onClick={() => setShowLogin(true)}
+        >
+          <span className="skew-x-[-45deg]">LOG IN</span>
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default CardNavigation;
